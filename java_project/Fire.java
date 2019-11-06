8aa7fc3de76ec6381d896fe2d3bd05d786ee2712
githubhaetsal1007 <57422032+githubhaetsal1007@users.noreply.github.com>
2019-11-06 18:10:17 +0900

    Add files via upload

--- java_project/Fire.java
+++ java_project/Fire.java
@@ -7,10 +7,42 @@ public class Fire {
 		int num2=20;
 		int num3=30;
 		num1=num2=num3;
+		System.out.println(" 문제 1 ");
 		System.out.println("num1 : "+num1);
 		System.out.println("num2 : "+num2);
 		System.out.println("num3 : "+num3);
 		// 가장 먼저 num3이 num2에 대입되고, num2의 값이 30으로 된 후에 num1에 대입되어서 모두 30이 됨
-
+		
+		System.out.println(" 문제 2 ");
+		System.out.println("결과 : "+(((25*5)+(36-4))-72)/5);
+		
+		System.out.println(" 문제 3 ");
+		int a=3;
+		int b=6;
+		int c=9;
+		int d=12;
+		int a1=a+b;
+		int a2=a+b+c;
+		int a3=a+b+c+d;
+		System.out.println("1 : "+a1);
+		System.out.println("2 : "+a2);
+		System.out.println("3 : "+a3);
+		
+		System.out.println(" 문제 4 ");
+		a = (((25*5)+(36-4))-72)/5;
+		b = (((25*5)+(36-4))-71)/4;
+		c = (128/4)*2;
+		
+		if(a>b) 
+		{
+			if(b>c) 
+				{
+				System.out.println(" true ");
+				}
+		}
+		else {
+			System.out.println(" false ");
+		}
+		
 }
 }
