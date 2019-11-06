432820d757152229539552e8d8158334a054dc25
githubhaetsal1007 <57422032+githubhaetsal1007@users.noreply.github.com>
2019-11-06 17:50:37 +0900

    문제 1번 풀이입니다.
    문제 1번 풀이입니다.

--- java_project/Fire.java
+++ java_project/Fire.java
@@ -3,7 +3,14 @@ package test;
 public class Fire {
 	public static void main(String args[]) 
 	{
-		System.out.println("결과 : "+(((25*5)+(36-4))-72)/5);
+		int num1=10;
+		int num2=20;
+		int num3=30;
+		num1=num2=num3;
+		System.out.println("num1 : "+num1);
+		System.out.println("num2 : "+num2);
+		System.out.println("num3 : "+num3);
+		// 가장 먼저 num3이 num2에 대입되고, num2의 값이 30으로 된 후에 num1에 대입되어서 모두 30이 됨
 
 }
 }
