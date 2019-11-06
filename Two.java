378eb1c1fff563994c9bde5cbbbae69699e0e52c
githubhaetsal1007 <sumagen1007@gmail.com>
2019-11-06 10:29:38 +0900

    ddd
    ddd

---
+++ Two.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Two {
+	public static void main(String[] args) {
+		
+		Four (1,2);
+	}
+	public static void Four(int a, int b) {
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.print("몫: "+a/b+" ");
+		System.out.println("나머지 "+a%b);
+		
+	}
+	
+}
